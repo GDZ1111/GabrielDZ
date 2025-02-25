19f63ac1c8ebb6d17d917020143347c3b87e103f
GDZ1111 <gaby.s160@gmail.com>
2025-02-26 00:41:10 +0100

    Create index.php

---
+++ instituto/index.php
@@ -0,0 +1,17 @@
+<?php
+// index.php - Menú de navegación
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Menú Principal</title>
+</head>
+<body>
+    <h1>Menú Principal</h1>
+    <ul>
+        <li><a href='alumnos.php'>Gestión de Alumnos</a></li>
+        <li><a href='profesores.php'>Gestión de Profesores</a></li>
+        <li><a href='busquedas.php'>Consultas</a></li>
+    </ul>
+</body>
+</html>
